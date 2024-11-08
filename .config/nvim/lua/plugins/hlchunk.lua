0e9b30fb5f35e9c4db1d474be4639f010c36bdff
Leerv474 <roman.lee474@gmail.com>
2024-11-08 22:57:44 +0300

    quick-git update

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -1,49 +1,27 @@
----@diagnostic disable: missing-fields
 return {
-    "shellraining/hlchunk.nvim",
-    event = { "BufReadPre", "BufNewFile" },
+	"shellRaining/hlchunk.nvim",
+	event = { "BufReadPre", "BufNewFile" },
 
-    config = function()
-        local hlchunk = require("hlchunk")
-        hlchunk.setup({
-            context = {},
-            chunk = {
+	config = function()
+		require("hlchunk").setup({
+			chunk = {
+				enable = true,
+				style = {
+					{ fg = "#a6adc8" },
+					{ fg = "#f38ba8" }, -- this fg is used to highlight wrong chunk
+				},
+			},
+			indent = {
                 enable = true,
-                notify = true,
-                use_treesitter = true,
-                chars = {
-                    horizontal_line = "─",
-                    vertical_line = "│",
-                    left_top = "╭",
-                    left_bottom = "╰",
-                    right_arrow = ">",
-                },
-                style = {
-                    { fg = "#a6adc8" },
-                    { fg = "#f38ba8" }, -- this fg is used to highlight wrong chunk
-                },
-                textobject = "",
-                max_file_size = 1024 * 1024 * 1024,
-                error_sign = true,
-            },
-
-            indent = {
-                enable = true,
-                use_treesitter = true,
                 chars = {
                     "┊",
                 },
-            },
-
-            line_num = {
-                enable = true,
-                use_treesitter = true,
-                style = "#a6adc8",
-            },
-
-            blank = {
-                enable = false,
-            },
-        })
-    end,
+            },line_num = {
+				enable = true,
+				use_treesitter = true,
+				style = "#a6adc8",
+			},
+		})
+	end,
 }
+
